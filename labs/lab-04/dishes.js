02bab4c8e3473904247b3b791c56a5c62b76b679
Egor Deev <egordeev.ru@gmail.com>
2025-11-19 11:27:06 +0300

    LAB-04

---
+++ labs/lab-04/dishes.js
@@ -0,0 +1,146 @@
+const dishes = [
+    {
+        keyword: "mushroom-soup",
+        name: "Грибной крем-суп",
+        price: 280,
+        category: "soup",
+        count: "300 мл",
+        image: "img/soup/mushroom-soup.jpg"
+    },
+    {
+        keyword: "beet-soup",
+        name: "Традиционный борщ",
+        price: 240,
+        category: "soup",
+        count: "350 мл",
+        image: "img/soup/beet-soup.jpg"
+    },
+    {
+        keyword: "chicken-noodle-soup",
+        name: "Куриный бульон с лапшой",
+        price: 220,
+        category: "soup",
+        count: "350 мл",
+        image: "img/soup/chicken-noodle-soup.jpg"
+    },
+    {
+        keyword: "minestrone-soup",
+        name: "Итальянский суп минестроне",
+        price: 260,
+        category: "soup",
+        count: "350 мл",
+        image: "img/soup/minestrone-soup.jpg"
+    },
+    {
+        keyword: "tomato-soup",
+        name: "Томатный крем-суп",
+        price: 250,
+        category: "soup",
+        count: "300 мл",
+        image: "img/soup/tomato-soup.jpg"
+    },
+    {
+        keyword: "pumpkin-soup",
+        name: "Тыквенный суп с имбирем",
+        price: 270,
+        category: "soup",
+        count: "300 мл",
+        image: "img/soup/pumpkin-soup.jpg"
+    },
+    {
+        keyword: "vegan-bowl-chickpeas",
+        name: "Веганский боул с нутом",
+        price: 390,
+        category: "main-course",
+        count: "350 г",
+        image: "img/main-dishes/vegan-bowl-chickpeas.jpg"
+    },
+    {
+        keyword: "grilled-chicken-vegetables",
+        name: "Куриная грудка с овощами гриль",
+        price: 450,
+        category: "main-course",
+        count: "350 г",
+        image: "img/main-dishes/grilled-chicken-vegetables.jpg"
+    },
+    {
+        keyword: "salmon-quinoa-avocado",
+        name: "Лосось с киноа и авокадо",
+        price: 620,
+        category: "main-course",
+        count: "380 г",
+        image: "img/main-dishes/salmon-quinoa-avocado.jpg"
+    },
+    {
+        keyword: "seafood-pasta",
+        name: "Паста с морепродуктами",
+        price: 550,
+        category: "main-course",
+        count: "330 г",
+        image: "img/main-dishes/seafood-pasta.jpg"
+    },
+    {
+        keyword: "teriyaki-chicken-noodles",
+        name: "Рисовая лапша с курицей терияки",
+        price: 420,
+        category: "main-course",
+        count: "340 г",
+        image: "img/main-dishes/teriyaki-chicken-noodles.jpg"
+    },
+    {
+        keyword: "beef-salad",
+        name: "Теплый салат с говядиной",
+        price: 490,
+        category: "main-course",
+        count: "320 г",
+        image: "img/main-dishes/beef-salad.jpg"
+    },
+    {
+        keyword: "berry-juice",
+        name: "Ягодный морс",
+        price: 120,
+        category: "drink",
+        count: "300 мл",
+        image: "img/drinks/berry-juice.jpg"
+    },
+    {
+        keyword: "water",
+        name: "Минеральная вода",
+        price: 80,
+        category: "drink",
+        count: "500 мл",
+        image: "img/drinks/water.jpg"
+    },
+    {
+        keyword: "spinach-smoothie",
+        name: "Зеленый смузи с шпинатом",
+        price: 180,
+        category: "drink",
+        count: "350 мл",
+        image: "img/drinks/spinach-smoothie.jpg"
+    },
+    {
+        keyword: "ginger-lemonade",
+        name: "Имбирный лимонад",
+        price: 140,
+        category: "drink",
+        count: "300 мл",
+        image: "img/drinks/ginger-lemonade.jpg"
+    },
+    {
+        keyword: "mango-smoothie",
+        name: "Смузи манго-маракуйя",
+        price: 170,
+        category: "drink",
+        count: "350 мл",
+        image: "img/drinks/mango-smoothie.jpg"
+    },
+    {
+        keyword: "orange-juice",
+        name: "Свежевыжатый апельсиновый сок",
+        price: 150,
+        category: "drink",
+        count: "300 мл",
+        image: "img/drinks/orange-juice.jpg"
+    }
+];
